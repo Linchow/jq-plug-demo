0f55fa4f4f8e98fb04fae98c20dc12c9d43ee43b
cuilinchao <1162321135@qq.com>
2018-12-29 15:08:49 +0800

    artTemplate的用法

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,15 @@
 
 requirejs.config({
     paths:{
-        jquery:['lib/jquery.min'],
+        jquery: ['lib/jquery.min'],
+        highlight: ['lib/highlight.pack'],
+        layer:['lib/layer'],
+        template:['lib/template'],
+        datepickr:['lib/datepicker.min'],
+        page:['lib/page'],
+        scroll:["lib/yi-scroll"],
+        qtip:["lib/jquery.qtip.min"],
+        form:['lib/jquery.form'],
     },
     urlArgs: "version=" +  (new Date()).getTime()
 })
@@ -13,10 +21,39 @@ require(['jquery'],function(){
     function init(){
         var routeId = $('.js-route-flag'); // 唯一标示
 
-        // 登录
-        if(routeId.hasClass("js-login")){
-            require(["../../static/js/login/login.js"],function(login){
-                login.init();
+        if(routeId.hasClass("js-demo1")){
+            require(["../js/demo1"],function(demo){
+                demo.init();
+            })
+        }
+        if(routeId.hasClass("js-demo2")){
+            require(["../js/demo2"],function(demo){
+                demo.init();
+            })
+        }
+        if(routeId.hasClass("js-demo3")){
+            require(["../js/demo3"],function(demo){
+                demo.init();
+            })
+        }
+        if(routeId.hasClass("js-demo4")){
+            require(["../js/demo4"],function(demo){
+                demo.init();
+            })
+        }
+        if(routeId.hasClass("js-demo5")){
+            require(["../js/demo5"],function(demo){
+                demo.init();
+            })
+        }
+        if(routeId.hasClass("js-demo6")){
+            require(["../js/demo6"],function(demo){
+                demo.init();
+            })
+        }
+        if(routeId.hasClass("js-demo7")){
+            require(["../js/demo7"],function(demo){
+                demo.init();
             })
         }
     }
